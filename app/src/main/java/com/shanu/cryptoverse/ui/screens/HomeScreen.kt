f4bcda2d5799154e15814a27df79115df9ee5ed2
Shoury Sharma <shanuu12e@gmail.com>
2021-11-20 11:29:46 +0530

    feat: Added collapsed topbar and list in cryptos

--- app/src/main/java/com/shanu/cryptoverse/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/shanu/cryptoverse/ui/screens/HomeScreen.kt
@@ -6,10 +6,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -28,33 +27,74 @@ fun HomeScreen() {
                 fontSize = 24.sp,
                 fontWeight = FontWeight.SemiBold
             )
-            Card (modifier = Modifier
-                .fillMaxWidth()
-                .height(88.dp)){
-                Column (modifier = Modifier.padding(12.dp).fillMaxWidth()){
-                    Row {
-                        Text(
-                            "$300 B",
-                            color = MaterialTheme.colors.primaryVariant,
-                            fontSize = 32.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                    Row(
-                        horizontalArrangement = Arrangement.End,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(
-                            "Market Cap",
-                            fontWeight = FontWeight.SemiBold
-                        )
-                    }
-                }
+            MarketCapCard()
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HomeHalfCard(text = "Markets", number = "83122")
+                HomeHalfCard(text = "Exchanges", number = "373")
             }
-            Row() {
-
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                HomeHalfCard(text = "Coins", number = "12616")
+                HomeHalfCard(text = "Volume", number = "104B")
             }
 
+        }
+    }
+}
+
+@Composable
+fun MarketCapCard() {
+    Card (modifier = Modifier
+        .fillMaxWidth()
+        .height(88.dp)){
+        Column (modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()){
+            Row {
+                Text(
+                    "$300 B",
+                    color = MaterialTheme.colors.primaryVariant,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    "Market Cap",
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeHalfCard(text:String, number: String) {
+    Card(modifier = Modifier
+        .width(180.dp)
+        .height(120.dp)) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+                ){
+            Text(
+                text = number,
+                fontSize = 28.sp,
+                color = MaterialTheme.colors.primaryVariant,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = text,
+                fontWeight = FontWeight.SemiBold
+            )
         }
     }
 }
--- app/src/main/java/com/shanu/cryptoverse/ui/screens/NewsScreen.kt
+++ app/src/main/java/com/shanu/cryptoverse/ui/screens/NewsScreen.kt
@@ -1,17 +1,49 @@
 package com.shanu.cryptoverse.ui.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @Composable
 fun NewsScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize()
+    val state = rememberCollapsingToolbarScaffoldState()
+    CollapsingToolbarScaffold(
+        state = state,
+        scrollStrategy = ScrollStrategy.EnterAlways,
+        modifier=Modifier,
+        toolbar = {
+            val textSize = (18 + (30 - 18) * state.toolbarState.progress).sp
+
+            Box(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.primary)
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .pin()
+            )
+
+            Text(
+                text = "Crypto News",
+                modifier = Modifier
+                    .road(Alignment.CenterStart, Alignment.BottomEnd)
+                    .padding(60.dp, 16.dp, 16.dp, 16.dp),
+                color = Color.White,
+                fontSize = textSize
+            )
+
+        }
     ) {
-        Text("Hello Home")
+
     }
 }
